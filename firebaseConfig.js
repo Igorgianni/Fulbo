2b83c1441214e40cecf4a5716bbfc71d667382b6
Igorgianni <126618797+Igorgianni@users.noreply.github.com>
2024-11-25 17:20:16 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,20 @@
+// Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  databaseURL: "TU_DATABASE_URL",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAaV1obh7GmbdCoOF3kX89L8tPMyy9VhIw",
+  authDomain: "fulbo21hs.firebaseapp.com",
+  projectId: "fulbo21hs",
+  storageBucket: "fulbo21hs.firebasestorage.app",
+  messagingSenderId: "117038985535",
+  appId: "1:117038985535:web:c8f0e4fc76dc526337a38a",
+  measurementId: "G-0W1BKHCM9W"
 };
 
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Inicializar servicios específicos si los necesitas
 const database = firebase.database();
 const auth = firebase.auth();
+
+// No podemos usar analytics con la versión actual del SDK, así que lo omitimos
+
