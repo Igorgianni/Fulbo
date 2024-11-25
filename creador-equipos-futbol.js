3858506735687df7aad9c1224a64b6b5ee043ca9
Igorgianni <126618797+Igorgianni@users.noreply.github.com>
2024-11-25 16:37:37 -0300

    Update creador-equipos-futbol.js

--- creador-equipos-futbol.js
+++ creador-equipos-futbol.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
 import { database, auth } from './firebaseConfig';
 import { ref, set, get, update } from "firebase/database";
 import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
